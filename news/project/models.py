4f01481a4bbf2ec7dc4594e1f03754ec9e4f2690
Papuqq <143436977+Papuqq@users.noreply.github.com>
2023-11-18 12:40:15 +0300

    D4

---
+++ news/project/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.views.generic import ListView, DetailView
+from django.urls import reverse
+
+
+# Товар для нашей витрины
+class Post(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        added_at=models.DateTimeField(
+            auto_now=True,
+        )
+    )
+    description = models.TextField()
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0)],
+    )
+    # поле категории будет ссылаться на модель категории
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='news',  # все продукты в категории будут доступны через поле products
+    )
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.id = None
+
+    def __str__(self):
+        return f'{self.name.title()}: {self.description[:20]}'
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name.title()
+
