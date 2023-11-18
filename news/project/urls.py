3a2d732a302e9898e4afdee179bd79914975af84
Papuqq <143436977+Papuqq@users.noreply.github.com>
2023-11-18 12:38:26 +0300

    Delete news directory

--- news/project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import PostList, PostDetail
-
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
-
-]
